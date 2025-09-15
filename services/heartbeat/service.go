5f3dbf1c996ede8c8e38e9769b9f275781f4b811
corp0529 <john.cleasby@gmail.com>
2025-09-15 15:29:58 +0100

    Connected bus and config

--- services/heartbeat/service.go
+++ services/heartbeat/service.go
@@ -3,27 +3,46 @@ package heartbeat
 import (
 	"context"
 	"time"
+
+	"devicecode-go/bus"
 )
+
+var topicConfigHeartbeat = bus.Topic{"config", "heartbeat"}
 
 type Service struct {}
 
-func (s *Service) serviceLoop(ctx context.Context) {
+func (s *Service) serviceLoop(ctx context.Context, conn *bus.Connection) {
+	cfgSub := conn.Subscribe(topicConfigHeartbeat)
+	defer conn.Unsubscribe(cfgSub)
+
 	tick := time.NewTicker(1 * time.Second)
 	defer tick.Stop()
 
-	for t := range tick.C {
+	// loop until context is cancelled, respond to tick and config changes
+	for {
 		select {
 		case <-ctx.Done():
+			println("Info: heartbeat service stopping")
 			return
-		default:
+		case t := <-tick.C:
+			println("Info:", t.Format("15:04:05"), "Heartbeat")
+		case msg := <-cfgSub.Channel():
+			println("Info:", "Received config message:", msg.Payload)			
+			// Change tick interval if needed
+			if m, ok := msg.Payload.(map[string]any); ok {
+				if iv, ok := m["interval"]; ok {
+					if interval, ok := iv.(float64); ok {
+						tick.Reset(time.Duration(interval) * time.Second)
+						println("Info:", "Heartbeat interval set to", interval, "seconds")
+					}
+				}
+			}
 		}
-
-		println("Info:", t.Format("15:04:05"), "Heartbeat")
 	}
 }
 
 // Start the heartbeat service.
-func (s *Service) Start(ctx context.Context) error {
-	go s.serviceLoop(ctx)
+func (s *Service) Start(ctx context.Context, conn *bus.Connection) error {
+	go s.serviceLoop(ctx, conn)
 	return nil
 }
